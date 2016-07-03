09ccbd9d465c64f34dbcbca3b33f2a2d065167f7
masakkuma <golden.forest627@gmail.com>
2016-07-03 15:43:12 +0900

    first commit

---
+++ src/HelloInteliJIDEA.java
@@ -0,0 +1,9 @@
+/**
+ * Created by masakkuma on 2016/07/03.
+ */
+public class HelloInteliJIDEA {
+
+    public static void main(String args[]) {
+        System.out.print("Hello InteliJ IDEA");
+    }
+}
